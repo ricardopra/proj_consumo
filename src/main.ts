e2149513825cec9e0e1f9f76f50f73e6e027e8a1
ricardopra <ricardopra@yahoo.com.br>
2024-11-01 13:48:18 -0300

    1.13

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  return app; // Apenas retorne o app
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);  
+  app.enableCors();  
+  app.useStaticAssets(join(__dirname, '..', 'public'));  
+  await app.listen(3000);
 }
-
 bootstrap();
